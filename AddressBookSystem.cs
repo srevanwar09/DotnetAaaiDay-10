901d37a60e9a09295da7c5270839112779edc3fa
srevanwar09 <108645553+srevanwar09@users.noreply.github.com>
2022-09-09 19:47:04 +0530

    Update AddressBookSystem.cs

--- AddressBookSystem.cs
+++ AddressBookSystem.cs
@@ -1,62 +1,45 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using AddressBookSystem;
 
-namespace AddressBookSystem
+Console.WriteLine("Welcome to AddressBook Program.");
+AddressBook addressBook = new AddressBook(); // creating the object of AddressBook class
+int option = 0;
+while(option != 8) //while loop is used so user can enter option of which task to perform and it will execute till it get value 3
 {
-    class Person
+    Console.WriteLine("-------------------------------------------");
+    Console.WriteLine("Press 1 for add contact.\nPress 2 for list the contact.\nPress 3  to edit the contact.");
+    Console.WriteLine("Press 4 to delete the contact.\nPress 5 to add Unique contact.");
+    Console.WriteLine("Press 6 to display Unique Contacts.\nPress 7 to search a contact.\nPress 8 to exit");
+    Console.WriteLine("Please enter option number: ");
+    option = int.Parse(Console.ReadLine()); // taking value of option through console
+
+    switch(option)
     {
-        public string FirstName { get; set; }
-        public string LastName { get; set; }
-        public int PhoneNumber { get; set; }
-        public string Email { get; set; }
-        public string[] Address { get; set; }
-    }
-    internal class AddressBook
-    {
-        public List<Person> People = new List<Person>(); // object is created of list class of type Person
-        public void addContact() // with this method the values are taken from user through console
-        {
-            Person person = new Person(); // creating a object of person class to assign the values received from user 
-            Console.Write("Enter the First Name: ");
-            person.FirstName = Console.ReadLine();
-            Console.Write("Enter the Last Name: ");
-            person.LastName = Console.ReadLine();
-            Console.Write("Enter the Mobile number: ");
-            person.PhoneNumber = int.Parse(Console.ReadLine());
-            Console.Write("Enter the Email ID: ");
-            person.Email = Console.ReadLine();
-            string[] add = new string[4];
-            Console.Write("Enter the Address: ");
-            add[0] = Console.ReadLine();
-            Console.Write("Enter the City: ");
-            add[1] = Console.ReadLine();
-            Console.Write("Enter the State: ");
-            add[2] = Console.ReadLine();
-            Console.Write("Enter the Zipcode: ");
-            add[3] = Console.ReadLine();
-            person.Address = add;
-            People.Add(person); // with add method we are adding the contact in list People
-        }
-        public void printContact(Person person) // in this method, we are printing all the details
-        {
-            Console.WriteLine("Full name : " + person.FirstName + " " + person.LastName);
-            Console.WriteLine("Mobile number : " + person.PhoneNumber);
-            Console.WriteLine("Email ID : " + person.Email);
-            Console.WriteLine("Address : " + person.Address[0]);
-            Console.WriteLine("City : " + person.Address[1]);
-            Console.WriteLine("State : " + person.Address[2]);
-            Console.WriteLine("Zipcode : " + person.Address[3]);
-        }
-        public void listContact()
-        {
-            Console.WriteLine("Following is your Contact List:");
-            foreach (var person in People) // using foreach loop, it will print the contact stored in People list one by one by calling printContact method
-            {
-                printContact(person);
-            }
-        }
+        case 1: // if option value 1 is entered then it matches with this case and user will be able to add contact
+            addressBook.addContact();
+            break;
+        case 2: //if option value 2 is entered then it matches with this case and user will get the list of contact store in List 
+            addressBook.listContact();
+            break;
+        case 3:// if option value 3 is entered then it matches with this case and user will be able to edit the contact
+            addressBook.editContact();
+            break;
+        case 4:// if option value 4 is entered then it matches with this case and user will be able to delete the contact
+            addressBook.deleteContact();
+            break;
+        case 5: // if option 5 is entered then it will ask user to enter first name and that contact will be added in unique addressbook
+            addressBook.AddUniqueContact();
+            break;
+        case 6: // if option 6 is entered then it will display the unique contacts from dictionary
+            addressBook.displayUniqueContact();
+            break;
+        case 7: //option 7 is entered then it will ask for city or state to enter and search the contact
+            addressBook.SearchContact();
+            break;
+        case 8:// if option value 7 is entered then it matches with this case and it will exit the code after displaying message
+            Console.WriteLine("Exiting from Program.");
+            break;
+        default: //if all the above cases doesn't match then it will print below message
+            Console.WriteLine("Wrong option.");
+            break;
     }
 }
